09b98fc82a381134de27ca078cfb3318d08848a2
Jo Kroese <jo@jokroese.com>
2025-10-07 14:57:24 +0200

    feat: add favicon

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,33 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
-import svelte from '@astrojs/svelte';
+import svelte from "@astrojs/svelte";
+
+import favicons from "astro-favicons";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
 
-  integrations: [svelte()]
+  integrations: [
+    svelte(),
+    favicons({
+      input: "public/favicon.svg",
+      name: "Creando Futuronan",
+      short_name: "Creando",
+      icons: {
+        favicons: true,
+        android: true,
+        appleIcon: true,
+        appleStartup: false,
+        windows: true,
+        yandex: true,
+      },
+      withCapo: true,
+    }),
+  ],
 });
